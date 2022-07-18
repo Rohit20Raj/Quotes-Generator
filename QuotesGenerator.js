9f8d7fa1f768d8ef9a13162aeb4f08fd1ffe2f5f
Rohit <rohitrajvns2020@gmail.com>
2022-07-18 09:49:51 +0530

    first commit

---
+++ QuotesGenerator.js
@@ -0,0 +1,30 @@
+var quotes = [
+["The greatest glory in living lies not in never falling, but in rising every time we fall.", "-Nelson Mandela"],
+["The way to get started is to quit talking and begin doing.", "-Walt Disney"],
+["Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking.", "-Steve Jobs"],
+["If life were predictable it would cease to be life, and be without flavor.", "-Eleanor Roosevelt"],
+["If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.", "-Oprah Winfrey"],
+["If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", "-James Cameron"],
+["Life is what happens when you're busy making other plans.", "-John Lennon"]
+];
+
+function generateQuote() {
+    quote = Math.floor(Math.random() * quotes.length);
+    document.querySelector("#quote").innerText = '"'+quotes[quote][0]+'"';
+    document.querySelector("#person").innerText = quotes[quote][1].toUpperCase();
+}
+
+var quote = Math.floor(Math.random() * quotes.length);
+
+document.querySelector("#quote").innerText = '"'+quotes[quote][0]+'"';
+document.querySelector("#person").innerText = quotes[quote][1].toUpperCase();
+
+var newQuote = document.querySelector("#newQuote");
+
+newQuote.addEventListener('click', generateQuote);
+
+// console.log(screen.width, screen.height);
+var x = screen.width, y = screen.height;
+if(x < y){
+    document.body.style.backgroundImage = 'url("https://wallpaperaccess.com/full/1200732.jpg")';
+}
